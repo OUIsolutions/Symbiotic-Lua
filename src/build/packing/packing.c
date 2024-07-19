2569c7e536c2b005bf5ebc27866c3911b5f48e5e
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-07-19 01:14:55 -0300

    att

--- src/build/packing/packing.c
+++ src/build/packing/packing.c
@@ -11,7 +11,7 @@ void create_bins(){
             "\tbool is_binary;\n"
             "\tunsigned char *content;\n"
             "}Bin;\n"
-            "\tBin bins[] = {\n"
+            "Bin bins[] = {\n"
     );
     UniversalGarbage_add(garbage,stack.free,data);
 
@@ -21,23 +21,27 @@ void create_bins(){
     for(int i=0; i < all->size;i++) {
         stack.text(data,"\n\t{\n");
         char *item = all->strings[i];
-        stack.format(data,"\t.path=\"%s\"\n",item);
+        stack.format(data,"\t\t.path=\"%s\"",item);
         char *full_path = dtw.concat_path("bin/all",item);
         if(dtw.entity_type(full_path) == DTW_FILE_TYPE) {
+            stack.format(data,",\n");
             long size;
             bool is_binary;
             unsigned char *content = dtw.load_any_content(full_path,&size,&is_binary);
-            stack.format(data,".size=%d,\n",size);
-            stack.format(data,".is_binary=%s,\n",is_binary? "true":"false");
-            stack.format(data,"\t.content=\"");
+            stack.format(data,"\t\t.size=%d,\n",size);
+            stack.format(data,"\t\t.is_binary=%s,\n",is_binary? "true":"false");
+            stack.format(data,"\t\t.content=\"");
             parse_bin(data,content,size);
             stack.format(data,"\"\n");
 
         }
+        else {
+            stack.format(data,"\n");
+        }
         free(full_path);
-        stack.text(data,"};\n");
+        stack.text(data,"\t},\n");
     }
-    stack.text(data,"}\n");
+    stack.text(data,"};\n");
     dtw.write_string_file_content("c/bin.h",data->rendered_text);
 
     UniversalGarbage_free(garbage);
